da626d785a2579c917ed9986b19f9d444030f5be
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2017-02-12 10:06:45 +0100

    Prepared for polymer.

--- brunch-config.js
+++ brunch-config.js
@@ -62,6 +62,46 @@ exports.config = {
         modules: true,
         includePaths: ['node_modules/foundation/scss']
       }
+    },
+
+    polymer: {
+      vulcanize: { // A top-level vulcanize is the 'default' path for files that do not match any in 'paths'.
+        options: { // These are normal vulcanize options passed as-is.
+          abspath: "web/static/_wc",
+          stripComments: true
+        }
+      },
+      crisper: {  // A top-level crisper is the 'default' path for files that do not match any in 'paths'
+        disabled: false, // If true then the vulcanized file is not split.
+        options: {}, // These are normal crisper options passed as-is.
+      },
+      paths: {
+        // The key is matched to the end of the path, if this file in the key
+        // is being compiled now then the global culvanize and crisper are not
+        // not used at all.  This can also be a regex matcher.
+        "somefile.polymer" : {
+          vulcanize: {} // Specifies vulcanize's options, the global version is unused
+          // If one is undefined, like crisper here, then it has no settings
+          // used, not even the global will be used, this is fully distinct.
+        }
+      },
+      // copyPathsToPublic: { // A set of paths to copy.
+      //   paths: {
+      //     "webcomponents/_polymer": [ // Place in 'public' to copy to
+      //       "web/static/webcomponents/_polymer"
+      //       // List of files to copy from, if this is a directory then copy all
+      //       // the files in the directory, not the directory itself.
+      //     ]
+      //   },
+      //   // verbosity: If 0 then no logging, if 1 then single line summary, if 2
+      //   // then summary per directory recursed into, if 3 then each and every
+      //   // file that is copied is printed.
+      //   verbosity: 1,
+      //   // onlyChanged: If true then compares timestamps before copying, this is
+      //   // only useful when 'watch' is used, it will always copy files
+      //   // regardless when just doing a normal build.
+      //   onlyChanged: true
+      // }
     }
   },
 
