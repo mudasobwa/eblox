e396159b4d124966f968463eb2f976db8396007b
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2017-02-23 13:43:12 +0100

    Before data.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,17 @@
 defmodule Eblox.PageController do
   use Eblox.Web, :controller
+  require Logger
+
+  plug :generic_route
 
   def index(conn, _params) do
     render conn, "index.html"
+  end
+
+  defp generic_route(conn, _opts) do
+    Logger.info inspect(conn.path_info)
+    # assign(conn, :user, user)
+    conn |> put_flash(:info, "You must be logged in")
+#    conn
   end
 end
