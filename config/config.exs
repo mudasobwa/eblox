2a3317ebbc32c56be0012971e5f86de61e1ebdfd
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2019-03-18 09:41:21 +0100

    Deps update.

--- config/config.exs
+++ config/config.exs
@@ -10,14 +10,15 @@ config :eblox, Eblox.Web.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "s/mRgMjmyNXrdLQDFhCaqztfJGTRta5MUkBQw0tBrFRnd0JJDa5spjF+UyJrzDFk",
   render_errors: [view: Eblox.Web.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Eblox.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Eblox.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :phoenix, :json_library, Jason
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
