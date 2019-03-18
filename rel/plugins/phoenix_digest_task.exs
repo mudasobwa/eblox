811d317cd82f0b7309e3c072e8da5e127582d798
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2019-03-18 10:29:48 +0100

    Deps update.

--- rel/plugins/phoenix_digest_task.exs
+++ rel/plugins/phoenix_digest_task.exs
@@ -2,34 +2,36 @@ defmodule Eblox.PhoenixDigestTask do
   use Mix.Releases.Plugin
 
   def before_assembly(%Release{} = _release, _opts \\ []) do
-    info "before assembly!"
+    info("before assembly!")
+
     case System.cmd("npm", ["run", "deploy"]) do
       {output, 0} ->
-        info output
-        Mix.Task.run("phoenix.digest")
+        info(output)
+        Mix.Task.run("phx.digest")
         nil
+
       {output, error_code} ->
         {:error, output, error_code}
     end
   end
 
   def after_assembly(%Release{} = _release, _opts \\ []) do
-    info "after assembly!"
+    info("after assembly!")
     nil
   end
 
   def before_package(%Release{} = _release, _opts \\ []) do
-    info "before package!"
+    info("before package!")
     nil
   end
 
   def after_package(%Release{} = _release, _opts \\ []) do
-    info "after package!"
+    info("after package!")
     nil
   end
 
   def after_cleanup(%Release{} = _release, _opts \\ []) do
-    info "after cleanup!"
+    info("after cleanup!")
     nil
   end
 end
